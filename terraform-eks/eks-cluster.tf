c49b1398c58550be36abcdf7c502b6eab0e5084c
Vrashabh <86019029+Vrashabh-Sontakke@users.noreply.github.com>
2023-02-17 11:55:10 +0530

    create `eks-cluster.tf`

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -6,5 +6,21 @@ module "eks" {
 
     cluster_endpoint_public_access = true
 
-    vpc_id = module.vpc.vpc_id
+    vpc_id = module.myapp-vpc.vpc_id
+    subnet_ids = module.myapp-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        applicaiton = "myapp"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+
+            instance_types = ["t2.medium"]
+        }
+    }
 }
